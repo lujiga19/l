f4a0a16e40857718a9d5c1114f57cbcfb970d618
lujiga19 <150866403+lujiga19@users.noreply.github.com>
2023-12-09 16:29:13 -0600

    Add files via upload

---
+++ JavaScript/informacion.js
@@ -0,0 +1,32 @@
+function informacion(id) {
+
+    if(id=='img1'){
+    document.write ("Director: Nicholas McCarthy");
+    document.write("<br/>Produccion: Tara Farney");
+    document.write("<br/>Actores: Taylor Schilling & Colm Feore");
+    
+    }else if(id=='img2'){
+        document.write ("Director: Ryan Coogler");
+    document.write("<br/>Produccion: Kevin Faige");
+    document.write("<br/>Actores: Letitia Wright & Tenoch Huerta");
+    
+
+    }else if(id=='img3'){
+        document.write ("Director: James Cameron");
+    document.write("<br/>Produccion: James Cameron");
+    document.write("<br/>Actores: Sam Worthington & Zoe Saldana");
+    
+
+    }else if(id=='img4'){
+        document.write ("Director: Joss Whendon");
+    document.write("<br/>Produccion: Kevin Faige");
+    document.write("<br/>Actores: Robert Downey Jr & Chris Evans");
+    
+
+    }else if(id=='img5'){
+        document.write ("Director: Ruben Fleischer");
+    document.write("<br/>Produccion: Avi Arad");
+    document.write("<br/>Actores: Tom Hardy & Michelle Williams");
+    
+    }
+}
---
+++ JavaScript/redimensionar.js
@@ -0,0 +1,12 @@
+function redimensionarMas(id) {
+   var image= document.getElementById(id);
+   image.style.width = '390px';
+    image.style.height = '640px';
+}
+
+function redimensionarMenos(id){
+    var image= document.getElementById(id);
+   image.style.width = '195px';
+    image.style.height = '320px';
+}
+
